7078d7d18a2a9b91cad3d4ee2c5d4b81c8262628
Kaveesha Hettige <2021is046@stu.ucsc.cmb.ac.lk>
2023-12-11 10:59:55 +0530

    after merge

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
  // DB Params
- define('DB_HOST', 'localhost');
+ define('DB_HOST', 'localhost:3306');
  define('DB_USER', 'root');
  define('DB_PASS', '12345');
  define('DB_NAME', 'travelease');
