4d8625c1de4f2819f4a7e7998d449fd11499c260
Wikumbandara <wikumpreethika123@gmail.com>
2024-04-29 16:49:53 +0530

    Finished pop ups in view buttons of Business Manager

--- app/views/businessmanager/rejectedBookings.php
+++ app/views/businessmanager/rejectedBookings.php
@@ -151,9 +151,10 @@ include 'navigation.php';
                     <td><?php echo $booking->service_detail?></td>
                     <td><?php echo $booking->payment_amount; ?></td>
                     <td>
-                        <button class="view-button" onclick="openPopup(<?php echo $key; ?>)">
+                        <button class="view-button" onclick="openPopup();updatePopupDetails('<?php echo $booking->traveler_name; ?>', '<?php echo $booking->service_type; ?>', '<?php echo $booking->serviceprovider_name; ?>', '<?php echo $booking->startDate; ?>', '<?php echo $booking->endDate; ?>', '<?php echo $booking->service_detail; ?>', '<?php echo $booking->payment_amount; ?>')">
                             <i class='bx bx-show'></i>
                         </button>
+                    </td>
                 </tr>
 
             <?php endforeach; ?>
@@ -166,18 +167,19 @@ include 'navigation.php';
     <div class="more-content">
         <button class="next-page-btn">See More <i class='bx bx-chevron-right'></i></button>
     </div>
-
     <div class="popup" id="popup">
         <div class="popup-content">
             <span class="close" onclick="closePopup()">&times;</span>
             <!-- Add details about the booking here -->
-            <h2>Booking Details</h2>
+            <h2>Cancelled Booking Details</h2>
             <div id="profile-picture" class="profile-picture"></div>
-            <p id="guestName">Guest Name: </p>
-            <p id="providerType">Provider Type: </p>
-            <p id="providerName">Provider Name: </p>
-            <p id="paymentStatus">Payment Status: </p>
-            <!-- Add more details as needed -->
+            <p id="traveler_name">Guest Name: </p>
+            <p id="service_type">Service Type: </p>
+            <p id="serviceprovider_name">Service Provider: </p>
+            <p id="startDate">Start Date: </p>
+            <p id="endDate">End Date: </p>
+            <p id="service_detail">Service Details: </p>
+            <p id="payment_amount">Payment Amount: </p>
         </div>
     </div>
 
--- public/js/businessmanager/bookings.js
+++ public/js/businessmanager/bookings.js
@@ -1,58 +1,21 @@
 function openPopup() {
     document.getElementById('popup').style.display = 'block';
-    console.log('Popup opened.');
 }
 
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
 
-function updatePopupDetails(profilePicture, guestName, providerType, providerName, paymentStatus) {
-    // Update the profile picture
-    var profilePictureDiv = document.getElementById('profile-picture');
-    profilePictureDiv.style.backgroundImage = "url('" + profilePicture + "')";
-    profilePictureDiv.title = 'Profile Picture';
+function updatePopupDetails(traveler_name,service_type, serviceprovider_name,startDate,endDate,service_detail, payment_amount) {
+    console.log(traveler_name,service_type, serviceprovider_name,startDate,endDate,service_detail, payment_amount);
 
     // Update other details
-    document.getElementById('guestName').innerText = 'Guest Name: ' + guestName;
+    document.getElementById('traveler_name').innerText = 'Traveler Name: ' + traveler_name;
+    document.getElementById('service_type').innerText = 'Service Type: ' + service_type;
+    document.getElementById('serviceprovider_name').innerText = 'Service Provider: ' + serviceprovider_name;
+    document.getElementById('startDate').innerText = 'Start Date: ' + startDate;
+    document.getElementById('endDate').innerText = 'End Date: ' + endDate;
+    document.getElementById('service_detail').innerText = 'Service Detail: ' + service_detail;
+    document.getElementById('payment_amount').innerText = 'Payment Amount: ' + payment_amount;
 
-    // Translate provider type number to text
-    var providerTypeText;
-    switch (providerType) {
-        case '0':
-            providerTypeText = 'Admin';
-            break;
-        case '1':
-            providerTypeText = 'Traveler';
-            break;
-        case '2':
-            providerTypeText = 'Business Manager';
-            break;
-        case '3':
-            providerTypeText = 'Hotel';
-            break;
-        case '4':
-            providerTypeText = 'Transport Provider';
-            break;
-        case '5':
-            providerTypeText = 'Guide';
-            break;
-        default:
-            providerTypeText = 'Unknown';
-            break;
-    }
-
-    // Update provider type in the popup
-    var providerTypeElement = document.getElementById('providerType');
-    if (providerTypeElement) {
-        providerTypeElement.innerText = 'Provider Type: ' + providerTypeText;
-    } else {
-        console.error('Element with ID "providerType" not found.');
-    }
-
-    // Update provider name in the popup
-    document.getElementById('providerName').innerText = 'Provider Name: ' + providerName;
-
-    // Update payment status in the popup
-    document.getElementById('paymentStatus').innerText = 'Payment Status: ' + paymentStatus;
 }
